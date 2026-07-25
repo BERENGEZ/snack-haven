5427f51ba743e108a23fae8f4eba8f3d3dfbb4ef
BERENGEZ <BERENGEZ>
2026-07-25 19:42:11 +0200

    Updated Flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from slask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app) #In this case I allow the coss origin requests to communicate with the HTML and PYTHON
